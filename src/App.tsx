3c87ca5131696fbb0c5957912137040cb8554699
Fouzia Oreen <fouziaoreen@gmail.com>
2026-07-05 22:01:38 +0600

    file updated

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div className="App">
+    <h1 className="app-title">OrganicFarm — Modern Multi-Vendor Grocery Marketplace</h1>
+
+    <p>
+    <strong>A scalable multi-vendor grocery marketplace that empowers sellers to manage their own stores while providing customers with a seamless, secure, and modern online grocery shopping experience.</strong>
+    </p>
+    </div>
+  )
+}
+
+export default App
